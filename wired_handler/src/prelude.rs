3a36f331d4ff9b271993de93e92c1d2d3089745c
MarekWojt <marek.wojt@web.de>
2024-07-01 02:26:58 +0200

    remove unnecessary exports from prelude

--- wired_handler/src/prelude.rs
+++ wired_handler/src/prelude.rs
@@ -1,2 +1,6 @@
-pub use crate::context::*;
-pub use crate::state::*;
+pub use crate::context::{ContextBuilder, GetState};
+pub use crate::state::{
+    StateAsyncGet, StateAsyncGetCloned, StateAsyncGetMut, StateAsyncMutableGetMut,
+    StateAsyncMutableProvide, StateAsyncProvide, StateSyncGet, StateSyncGetCloned, StateSyncGetMut,
+    StateSyncMutableGetMut, StateSyncMutableProvide, StateSyncProvide,
+};
