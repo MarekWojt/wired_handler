9bad34ef85b5a9807420f56df7ba217dc58adbd1
MarekWojt <marek.wojt@web.de>
2025-12-23 03:54:55 +0100

    remove unnecessary as_derefs in test

--- wired_handler_hyper/src/data/path/test.rs
+++ wired_handler_hyper/src/data/path/test.rs
@@ -24,63 +24,63 @@ fn test() {
     {
         let mut ctx = context_with_path("");
         let remaining_path = ctx.remaining_path_mut();
-        assert_eq!(Some(""), remaining_path.peek().as_deref());
+        assert_eq!(Some(""), remaining_path.peek());
         assert_eq!(Some(""), remaining_path.next().as_deref());
-        assert_eq!(None, remaining_path.peek().as_deref());
+        assert_eq!(None, remaining_path.peek());
         assert_eq!(None, remaining_path.next().as_deref());
-        assert_eq!(None, remaining_path.peek().as_deref());
+        assert_eq!(None, remaining_path.peek());
         assert_eq!(None, remaining_path.next().as_deref());
     }
 
     {
         let mut ctx = context_with_path("/");
         let remaining_path = ctx.remaining_path_mut();
-        assert_eq!(Some(""), remaining_path.peek().as_deref());
+        assert_eq!(Some(""), remaining_path.peek());
         assert_eq!(Some(""), remaining_path.next().as_deref());
-        assert_eq!(None, remaining_path.peek().as_deref());
+        assert_eq!(None, remaining_path.peek());
         assert_eq!(None, remaining_path.next().as_deref());
-        assert_eq!(None, remaining_path.peek().as_deref());
+        assert_eq!(None, remaining_path.peek());
         assert_eq!(None, remaining_path.next().as_deref());
     }
 
     {
         let mut ctx = context_with_path("/test");
         let remaining_path = ctx.remaining_path_mut();
-        assert_eq!(Some("test"), remaining_path.peek().as_deref());
+        assert_eq!(Some("test"), remaining_path.peek());
         assert_eq!(Some("test"), remaining_path.next().as_deref());
-        assert_eq!(None, remaining_path.peek().as_deref());
+        assert_eq!(None, remaining_path.peek());
         assert_eq!(None, remaining_path.next().as_deref());
-        assert_eq!(None, remaining_path.peek().as_deref());
+        assert_eq!(None, remaining_path.peek());
         assert_eq!(None, remaining_path.next().as_deref());
     }
 
     {
         let mut ctx = context_with_path("/test/bla");
         let remaining_path = ctx.remaining_path_mut();
-        assert_eq!(Some("test"), remaining_path.peek().as_deref());
+        assert_eq!(Some("test"), remaining_path.peek());
         assert_eq!(Some("test"), remaining_path.next().as_deref());
-        assert_eq!(Some("bla"), remaining_path.peek().as_deref());
+        assert_eq!(Some("bla"), remaining_path.peek());
         assert_eq!(Some("bla"), remaining_path.next().as_deref());
-        assert_eq!(None, remaining_path.peek().as_deref());
+        assert_eq!(None, remaining_path.peek());
         assert_eq!(None, remaining_path.next().as_deref());
-        assert_eq!(None, remaining_path.peek().as_deref());
+        assert_eq!(None, remaining_path.peek());
         assert_eq!(None, remaining_path.next().as_deref());
     }
 
     {
         let mut ctx = context_with_path("/test/first/second/last");
         let remaining_path = ctx.remaining_path_mut();
-        assert_eq!(Some("test"), remaining_path.peek().as_deref());
+        assert_eq!(Some("test"), remaining_path.peek());
         assert_eq!(Some("test"), remaining_path.next().as_deref());
-        assert_eq!(Some("first"), remaining_path.peek().as_deref());
+        assert_eq!(Some("first"), remaining_path.peek());
         assert_eq!(Some("first"), remaining_path.next().as_deref());
-        assert_eq!(Some("second"), remaining_path.peek().as_deref());
+        assert_eq!(Some("second"), remaining_path.peek());
         assert_eq!(Some("second"), remaining_path.next().as_deref());
-        assert_eq!(Some("last"), remaining_path.peek().as_deref());
+        assert_eq!(Some("last"), remaining_path.peek());
         assert_eq!(Some("last"), remaining_path.next().as_deref());
-        assert_eq!(None, remaining_path.peek().as_deref());
+        assert_eq!(None, remaining_path.peek());
         assert_eq!(None, remaining_path.next().as_deref());
-        assert_eq!(None, remaining_path.peek().as_deref());
+        assert_eq!(None, remaining_path.peek());
         assert_eq!(None, remaining_path.next().as_deref());
     }
 
@@ -88,63 +88,63 @@ fn test() {
     {
         let mut ctx = context_with_path("");
         let remaining_path = ctx.remaining_path_mut();
-        assert_eq!(Some(""), remaining_path.peek_back().as_deref());
+        assert_eq!(Some(""), remaining_path.peek_back());
         assert_eq!(Some(""), remaining_path.next_back().as_deref());
-        assert_eq!(None, remaining_path.peek_back().as_deref());
+        assert_eq!(None, remaining_path.peek_back());
         assert_eq!(None, remaining_path.next_back().as_deref());
-        assert_eq!(None, remaining_path.peek_back().as_deref());
+        assert_eq!(None, remaining_path.peek_back());
         assert_eq!(None, remaining_path.next_back().as_deref());
     }
 
     {
         let mut ctx = context_with_path("/");
         let remaining_path = ctx.remaining_path_mut();
-        assert_eq!(Some(""), remaining_path.peek_back().as_deref());
+        assert_eq!(Some(""), remaining_path.peek_back());
         assert_eq!(Some(""), remaining_path.next_back().as_deref());
-        assert_eq!(None, remaining_path.peek_back().as_deref());
+        assert_eq!(None, remaining_path.peek_back());
         assert_eq!(None, remaining_path.next_back().as_deref());
-        assert_eq!(None, remaining_path.peek_back().as_deref());
+        assert_eq!(None, remaining_path.peek_back());
         assert_eq!(None, remaining_path.next_back().as_deref());
     }
 
     {
         let mut ctx = context_with_path("/test");
         let remaining_path = ctx.remaining_path_mut();
-        assert_eq!(Some("test"), remaining_path.peek_back().as_deref());
+        assert_eq!(Some("test"), remaining_path.peek_back());
         assert_eq!(Some("test"), remaining_path.next_back().as_deref());
-        assert_eq!(None, remaining_path.peek_back().as_deref());
+        assert_eq!(None, remaining_path.peek_back());
         assert_eq!(None, remaining_path.next_back().as_deref());
-        assert_eq!(None, remaining_path.peek_back().as_deref());
+        assert_eq!(None, remaining_path.peek_back());
         assert_eq!(None, remaining_path.next_back().as_deref());
     }
 
     {
         let mut ctx = context_with_path("/test/bla");
         let remaining_path = ctx.remaining_path_mut();
-        assert_eq!(Some("bla"), remaining_path.peek_back().as_deref());
+        assert_eq!(Some("bla"), remaining_path.peek_back());
         assert_eq!(Some("bla"), remaining_path.next_back().as_deref());
-        assert_eq!(Some("test"), remaining_path.peek_back().as_deref());
+        assert_eq!(Some("test"), remaining_path.peek_back());
         assert_eq!(Some("test"), remaining_path.next_back().as_deref());
-        assert_eq!(None, remaining_path.peek_back().as_deref());
+        assert_eq!(None, remaining_path.peek_back());
         assert_eq!(None, remaining_path.next_back().as_deref());
-        assert_eq!(None, remaining_path.peek_back().as_deref());
+        assert_eq!(None, remaining_path.peek_back());
         assert_eq!(None, remaining_path.next_back().as_deref());
     }
 
     {
         let mut ctx = context_with_path("/test/first/second/last");
         let remaining_path = ctx.remaining_path_mut();
-        assert_eq!(Some("last"), remaining_path.peek_back().as_deref());
+        assert_eq!(Some("last"), remaining_path.peek_back());
         assert_eq!(Some("last"), remaining_path.next_back().as_deref());
-        assert_eq!(Some("second"), remaining_path.peek_back().as_deref());
+        assert_eq!(Some("second"), remaining_path.peek_back());
         assert_eq!(Some("second"), remaining_path.next_back().as_deref());
-        assert_eq!(Some("first"), remaining_path.peek_back().as_deref());
+        assert_eq!(Some("first"), remaining_path.peek_back());
         assert_eq!(Some("first"), remaining_path.next_back().as_deref());
-        assert_eq!(Some("test"), remaining_path.peek_back().as_deref());
+        assert_eq!(Some("test"), remaining_path.peek_back());
         assert_eq!(Some("test"), remaining_path.next_back().as_deref());
-        assert_eq!(None, remaining_path.peek_back().as_deref());
+        assert_eq!(None, remaining_path.peek_back());
         assert_eq!(None, remaining_path.next_back().as_deref());
-        assert_eq!(None, remaining_path.peek_back().as_deref());
+        assert_eq!(None, remaining_path.peek_back());
         assert_eq!(None, remaining_path.next_back().as_deref());
     }
 }
